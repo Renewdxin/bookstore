9e8b1df53477b0bd8776bc529a37c2450dcac0d2
Renewdxin <cissyrenxin@icloud.com>
2023-11-22 16:09:49 +0800

    11.22

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	// To define a route, we need the endpoint and the handler
 	// func(context *gin.Context){}: determines how we provide the data to the client
 	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
 	r.POST("/books", controllers.Createbook)
 	r.Run(":9090")
 }
