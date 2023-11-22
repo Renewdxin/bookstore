81f3a47bacfc3772398ec6973608d47f62f5d6d9
Renewdxin <cissyrenxin@icloud.com>
2023-11-22 15:11:28 +0800

    11.22

--- controllers/books.go
+++ controllers/books.go
@@ -8,6 +8,7 @@ import (
 
 func FindBooks(c *gin.Context) {
 	var books []models.Book
+	// Find all books matching the given conditions
 	models.DB.Find(&books)
 	c.JSON(http.StatusOK, gin.H{
 		"data": books,
